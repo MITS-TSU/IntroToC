252567a211088a01378a25f58274187f30b3beb3
ChickenLover <Romangg81@gmail.com>
2021-02-19 22:21:06 +0700

    add printf.c

---
+++ printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("Hello, world");
+
+    printf("Hello, world\n");
+
+    printf("%s - лучшая школа программирования\n", "ШСП");
+
+    printf("Язык СИ появился в %d году\n", 1972);
+    
+    printf("Нормальная температура тела - %f градусов Цельсия\n", 36.6);
+
+    return 0;
+}
